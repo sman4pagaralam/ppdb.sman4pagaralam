a2062c0573d93c2ae1b4a349a72e131a65a66d53
sman4pagaralam <boyc38675@gmail.com>
2026-05-25 07:45:39 +0700

    Update gas-proxy.js

--- api/gas-proxy.js
+++ api/gas-proxy.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  const GAS_URL = 'https://script.google.com/macros/s/AKfycbwMV3wQ8pIRicBrwEPSEe2HsK3AavEnOlezeDA0Ta0DuId4JiGQUkrdm4fzpPA35IWxEQ/exec';
+  const GAS_URL = 'https://script.google.com/macros/s/AKfycby-ESjEm9pedP2wlofsZp_mcrFVqujGQKmAFrIfxFyMim12ulm6AdBoCn8URnVNc3UglA/exec';
   
   try {
     let url = GAS_URL;
